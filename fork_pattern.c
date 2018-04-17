c607f475ef92876ccaa547e593771f9c1c7ca41e
JayRajM97 <jayraj.mka@gmail.com>
2018-04-18 01:03:35 +0530

    Program Completed

--- fork_pattern.c
+++ fork_pattern.c
@@ -13,6 +13,17 @@ int main()
 		exit(-1);
 	}
 
-	
+	pid_t ch = fork();
+
+	if(ch == 0) {
+		while(n > 0) {
+			printf("  %d  ", n);
+			n /= 2;
+		}
+		printf("\n");
+		exit(0);
+	}
+	wait(NULL);
+	return 0;
 }
 
